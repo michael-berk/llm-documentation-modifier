bee4c0364c6d9f45842e19200ee99a67bfedb0ad
Michael Berk <michael.berk@databricks.com>
2024-01-16 15:54:48 -0500

    Update match logic

--- extras/compare.py
+++ extras/compare.py
@@ -4,6 +4,8 @@ import difflib
 import pandas as pd
 import os
 
+DIRS_TO_NOT_SEARCH = {"/_modules/"}
+
 def _get_relative_file_name(full_path: str) -> str:
     return full_path.split("mlflow")[-1].replace("/docs/build/html/", "")
 
@@ -23,21 +25,20 @@ def diff_files(old_soup, new_soup):
         if (line.startswith("- ") or line.startswith("+ ")) and len(line[1:].strip()) > 0
     ]
 
-    for x in filtered_diff:
-        print(f"{x[0]}: {x[1]}")
+    return filtered_diff
+
 
 
 def compare_html_files(old_dir: str, new_dir: str):
+    paths_with_diffs = []
     for root, _, files in os.walk(old_dir):
         for file in files:
-            if file.endswith(".html"):
-                print(_get_relative_file_name(root + '/' + file))
-                print("------------------------------")
+            if file.endswith(".html") and not any([d in root for d in DIRS_TO_NOT_SEARCH]):
+                # print(_get_relative_file_name(root + '/' + file))
                 old_file_path = os.path.join(root, file)
                 new_file_path = os.path.join(
                     new_dir, os.path.relpath(old_file_path, old_dir)
                 )
-
                 if os.path.exists(new_file_path):
                     with open(old_file_path, "r") as old_file, open(
                         new_file_path, "r"
@@ -45,7 +46,21 @@ def compare_html_files(old_dir: str, new_dir: str):
                         old_soup = BeautifulSoup(old_file.read(), "html.parser")
                         new_soup = BeautifulSoup(new_file.read(), "html.parser")
 
-                        diff_files(old_soup, new_soup)
+
+                        diff = diff_files(old_soup, new_soup)
+                        if len(diff) > 0:
+                            paths_with_diffs.append(new_file_path)
+                            print(new_file_path)
+                            print("------------------------------")
+
+                            for x in diff:
+                                print(f"{x[0]}: {x[1]}")
+
+    print("\n"*5)
+    print("List of all files with diffs:")
+    print("------------------------------")
+    for p in paths_with_diffs:
+        print(p)
 
 
 @click.command()
